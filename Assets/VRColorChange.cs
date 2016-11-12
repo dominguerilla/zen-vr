997b8472a3dfab595663fd45d9fc0471acc3c551
Carlos Dominguez <Carlos Dominguez>
2016-11-12 07:46:12 -0500

    Adding everything

---
+++ Assets/VRColorChange.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+[RequireComponent(typeof(VRInteractiveItem))]
+public class VRColorChange : MonoBehaviour {
+
+    VRInteractiveItem vrItem;
+    Renderer materialRenderer;
+    
+    Material offMat;
+
+    [SerializeField]
+    Material onMat;
+
+    [SerializeField]
+    Material onDownMat;
+
+    [SerializeField]
+    Material onUpMat;
+
+
+    void Awake()
+    {
+        vrItem = GetComponent<VRInteractiveItem>();
+        materialRenderer = GetComponent<Renderer>();
+    }
+
+
+    void Start () {
+        offMat = materialRenderer.material;
+
+        vrItem.OnOver += HandleOnOver;
+        vrItem.OnOut += HandleOnOut;
+        vrItem.OnDown += HandleOnDown;
+        vrItem.OnUp += HandleOnUp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void HandleOnOut()
+    {
+        materialRenderer.material = offMat;
+    }
+
+    void HandleOnOver()
+    {
+        materialRenderer.material = onMat;
+    }
+
+    void HandleOnDown()
+    {
+        materialRenderer.material = onDownMat;
+    }
+
+    void HandleOnUp()
+    {
+        materialRenderer.material = onUpMat;
+    }
+
+    void HandleSwipeRight()
+    {
+
+    }
+
+    void HandleSwipeLeft()
+    {
+
+    }
+
+    void HandleSwipeUp()
+    {
+
+    }
+
+    void HandleSwipeDown()
+    {
+
+    }
+}
---
+++ Assets/VRTeleportPoint.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class VRTeleportPoint : MonoBehaviour {
+
+    [SerializeField]
+    VRInteractiveItem vrItem;
+
+    Renderer matRender;
+    Material offMat;
+    [SerializeField]
+    Material onMat;
+
+    [SerializeField]
+    Material clickMat;
+
+    [SerializeField]
+    Transform TeleportSpot;
+    Vector3 teleVector;
+
+    [SerializeField]
+    Transform teleportObj;
+
+    [SerializeField]
+    float defaultVerticalOffset = 1.0f;
+
+
+    void Awake()
+    {
+        matRender = GetComponent<Renderer>();
+        offMat = matRender.material;
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+        if (!TeleportSpot)
+        {
+            teleVector = this.gameObject.transform.up;
+        }else
+        {
+            teleVector = TeleportSpot.position;
+        }
+        
+        
+        vrItem.OnClick += Teleport;
+        vrItem.OnOver += OnOverHandle;
+        vrItem.OnOut += OnOffHandle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Teleport()
+    {
+        matRender.material = clickMat;
+        teleportObj.gameObject.transform.position = teleVector;
+    }
+
+    void OnOverHandle()
+    {
+        matRender.material = onMat;
+    }
+
+    void OnOffHandle()
+    {
+        matRender.material = offMat;
+    }
+}
